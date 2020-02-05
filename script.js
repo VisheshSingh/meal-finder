be3335c33c2d3e0dd210d40f28a8c3bbc87db443
Vishesh Singh <VisheshSingh16@gmail.com>
2020-02-05 16:40:47 -0600

    show single meal per page

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function searchMeal(e) {
             .map(meal => {
               return `<div class="meal">
             <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-            <div class="meal-info" data-mealId="${meal.mealId}">
+            <div class="meal-info" data-mealId="${meal.idMeal}">
             <h3>${meal.strMeal}</h3>
             </div>
             </div>`;
@@ -36,5 +36,60 @@ function searchMeal(e) {
     alert('Please enter a keyword to proceed.');
   }
 }
+
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+}
+
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  singleMeal.innerHTML = `
+    <div class="single-meal">
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+        <div class="single-meal-info">
+            ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+            ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+        </div>
+        <div class="main">
+            <p>${meal.strInstructions}</p>
+            <h2>Ingredients</h2>
+            <ul>
+                ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+            </ul>
+        </div>
+    </div>
+  `;
+}
+
 // Event Listeners
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', e => {
+  const mealEl = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+
+  const mealId = mealEl.dataset.mealid;
+  getMealById(mealId);
+});
